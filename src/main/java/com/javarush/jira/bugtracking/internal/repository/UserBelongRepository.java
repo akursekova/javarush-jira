1971f9b76d5b007371d4280dd325081fecb547ec
akursekova <kursek.alina.jr@gmail.com>
2023-05-27 19:06:57 +0200

    Added the ability to subscribe to tasks that are not assigned to the current user: I used the following logic for implementation. If user start to follow a task, new record should be added to the user_belong table, if user stops to follow the task, corresponding record will be deleted from user_belong table - added two new methods to TaskRestController: 1. subscribe: method creates new record in user_belong table 2.  unsubscribe: method deletes corresponding record from user_belong. - currently there is no way to define user_type_code by user_id, because of that by default "admin" user_type_code is set to all records, independently if it is admin or not. - UserBelong class updated, added @AllArgsConstructor annotation - added native query to UserBelongRepository, which returns id of the user_belong record by unique combination of user_id, task_is, user_type_code and object_type.

--- src/main/java/com/javarush/jira/bugtracking/internal/repository/UserBelongRepository.java
+++ src/main/java/com/javarush/jira/bugtracking/internal/repository/UserBelongRepository.java
@@ -2,9 +2,14 @@ package com.javarush.jira.bugtracking.internal.repository;
 
 import com.javarush.jira.bugtracking.internal.model.UserBelong;
 import com.javarush.jira.common.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface UserBelongRepository extends BaseRepository<UserBelong> {
+
+    @Query(value = "SELECT ub.id FROM user_belong ub WHERE ub.user_id=:userId AND ub.object_id=:taskId " +
+            "AND ub.object_type=:type AND ub.user_type_code=:userTypeCode", nativeQuery = true)
+    Long getUserBelongByUserIdAndObjectIdAndObjectType(Long userId, Long taskId, int type, String userTypeCode);
 
 }
--- src/main/java/com/javarush/jira/bugtracking/web/TaskRestController.java
+++ src/main/java/com/javarush/jira/bugtracking/web/TaskRestController.java
@@ -1,13 +1,18 @@
 package com.javarush.jira.bugtracking.web;
 
-import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
+import com.javarush.jira.bugtracking.internal.model.UserBelong;
+import com.javarush.jira.bugtracking.internal.repository.UserBelongRepository;
+import com.javarush.jira.bugtracking.to.ObjectType;
 import com.javarush.jira.bugtracking.to.TaskTagsTo;
+import com.javarush.jira.login.AuthUser;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,12 +24,28 @@ public class TaskRestController extends AbstractTaskController {
     public static final String REST_URL = "/api/tasks";
 
     @Autowired
-    TaskRepository taskRepository;
+    UserBelongRepository userBelongRepository;
 
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@Valid @RequestBody TaskTagsTo taskTagsTo) {
+    public void addTagsToTask(@Valid @RequestBody TaskTagsTo taskTagsTo) {
         super.update(taskTagsTo);
     }
+
+    @PostMapping("/{taskId}/watchers")
+    public ResponseEntity<UserBelong> subscribe(@PathVariable String taskId, @AuthenticationPrincipal AuthUser authUser) {
+        UserBelong userBelong = new UserBelong(Long.valueOf(taskId), ObjectType.TASK, authUser.id(), "admin"); //todo to add logic how to get user_type_code by user_id
+        UserBelong created = userBelongRepository.save(userBelong);
+
+        return ResponseEntity.ok().body(created);
+    }
+
+    @DeleteMapping("/{taskId}/watchers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void unsubscribe(@PathVariable String taskId, @AuthenticationPrincipal AuthUser authUser) {
+        Long userBelongId = userBelongRepository.getUserBelongByUserIdAndObjectIdAndObjectType(authUser.id(), Long.valueOf(taskId), ObjectType.TASK.ordinal(), "admin");
+        userBelongRepository.delete(userBelongId);
+    }
+
 }
